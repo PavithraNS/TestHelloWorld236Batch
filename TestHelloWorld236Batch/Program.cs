fdfcbaab706a09fe23add636dfba99c9294b8273
PavithraNS <nspavi1991@gmail.com>
2022-12-08 12:55:52 +0530

    Adding Static Variable concept

--- TestHelloWorld236Batch/Program.cs
+++ TestHelloWorld236Batch/Program.cs
@@ -19,18 +19,21 @@ namespace TestHelloWorld236Batch
             //TypesOfVariables types = new TypesOfVariables();
             //TypesOfVariables typesOfVariables = new TypesOfVariables(234);
 
-            Marks obj1 = new Marks();
-            obj1.engMarks = 90;
-            obj1.mathsMarks = 80;
-            obj1.phyMarks = 93;
-            obj1.MarksDeatils();
+            //Marks obj1 = new Marks();
+            //obj1.engMarks = 90;
+            //obj1.mathsMarks = 80;
+            //obj1.phyMarks = 93;
+            //obj1.MarksDeatils();
 
-            // second object
-            Marks obj2 = new Marks();
-            obj2.engMarks = 95;
-            obj2.mathsMarks = 70;
-            obj2.phyMarks = 90;
-            obj2.MarksDeatils();
+            //// second object
+            //Marks obj2 = new Marks();
+            //obj2.engMarks = 95;
+            //obj2.mathsMarks = 70;
+            //obj2.phyMarks = 90;
+            //obj2.MarksDeatils();
+
+            //StaticVariableConcept.number=23;
+            Console.WriteLine(StaticVariableConcept.number);
             Console.ReadLine();
         }
     }
